6e5c2a218b37f9b81a0d0c315fe6c86f4554e810
root <farahalmzini@gmail.com>
2025-06-26 15:26:29 +0000

    Added a shell.h file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+/* Standard Libraries */
+#include <stdio.h>      /* getline, perror */
+#include <stdlib.h>     /* malloc, free, exit, getenv */
+#include <unistd.h>     /* fork, execve, access, write */
+#include <string.h>     /* strtok, strdup, strlen, strcmp, snprintf */
+#include <sys/wait.h>   /* wait, waitpid */
+
+/* Global Environment */
+extern char **environ;
+
+/* Function Prototypes */
+
+#endif /* SHELL_H */
