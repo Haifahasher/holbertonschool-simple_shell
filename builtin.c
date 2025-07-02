a4ac0a7d918a015cf910262fca5b24cb748d8ec0
Haifahasher <Haifahasher@gmail.com>
2025-07-02 12:38:50 +0000

    update builtin.c and main.c

--- builtin.c
+++ builtin.c
@@ -3,12 +3,12 @@
 /**
  * handle_builtin - Checks and executes built-in commands
  * @args: Argument array
- * Return: 1 if a built-in was handled, 0 otherwise
+ * Return: 1 if a built-in was handled, 0 otherwise, -1 for exit
  */
 int handle_builtin(char **args)
 {
 	if (strcmp(args[0], "exit") == 0)
-		exit(0);
+		return (-1);
 	if (strcmp(args[0], "env") == 0)
 	{
 		print_env();
